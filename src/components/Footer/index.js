60ff923535a71a7ce9beee87c588894a762f71e1
devdesignerstudio <devdesignerstudio@protonmail.com>
2023-04-26 20:40:57 -0300

    atualizando links das redes socais do rodapé  da Alura

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -8,17 +8,20 @@ const Footer = () => {
       <section>
         <ul>
           <li>
-            <a href="facebook.com" target="_blank">
+            <a
+              href="https://www.facebook.com/AluraCursosOnline/"
+              target="_blank"
+            >
               <img src="./img/fb.png" alt="ícone do facebook" />
             </a>
           </li>
           <li>
-            <a href="twitter.com" target="_blank">
+            <a href="https://twitter.com/aluraonline" target="_blank">
               <img src="/img/tw.png" alt="ícone do twitter" />
             </a>
           </li>
           <li>
-            <a href="instagram.com" target="_blank">
+            <a href="https://instagram.com/aluraonline" target="_blank">
               <img src="/img/ig.png" alt="ícone do instagram" />
             </a>
           </li>
