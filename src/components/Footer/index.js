c80480c0a38c546c0e46be7b16cfb50088f41551
devdesignerstudio <devdesignerstudio@protonmail.com>
2023-04-26 20:58:50 -0300

    Mudança de links do rodapé contendo propriedade segura

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -10,18 +10,27 @@ const Footer = () => {
           <li>
             <a
               href="https://www.facebook.com/AluraCursosOnline/"
+              rel="noreferrer"
               target="_blank"
             >
               <img src="./img/fb.png" alt="ícone do facebook" />
             </a>
           </li>
           <li>
-            <a href="https://twitter.com/aluraonline" target="_blank">
+            <a
+              href="https://twitter.com/aluraonline"
+              rel="noreferrer"
+              target="_blank"
+            >
               <img src="/img/tw.png" alt="ícone do twitter" />
             </a>
           </li>
           <li>
-            <a href="https://instagram.com/aluraonline" target="_blank">
+            <a
+              href="https://instagram.com/aluraonline"
+              rel="noreferrer"
+              target="_blank"
+            >
               <img src="/img/ig.png" alt="ícone do instagram" />
             </a>
           </li>
@@ -31,7 +40,17 @@ const Footer = () => {
         <img src="./img/logo.png" alt="" />
       </section>
       <section>
-        <p>Desenvolvido por Alura.</p>
+        <p>
+          Desenvolvido por{" "}
+          <a
+            href="https://github.com/devdesignerstudio"
+            rel="noreferrer"
+            target="_blank"
+          >
+            DevDesignerStudio
+          </a>{" "}
+          no Curso Alura.
+        </p>
       </section>
     </footer>
   )
